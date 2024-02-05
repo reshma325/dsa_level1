6102c8960bfc94fc13e641edb13adaf114f22ada
omkar shinde <omkarshinde003@gmail.com>
2024-02-05 07:36:25 +0000

    Day1

---
+++ Day1/src/one.java
@@ -0,0 +1,8 @@
+/*
+* Taking input in Java, printing anything in console, learning how to take int, float, double, string input using Scanner class
+*Task:- take input from console number 1.5, a, 6 and print them in the console 1.5 $ a $ 6
+*
+* */
+
+public class one {
+}
---
+++ Day1/src/two.java
@@ -0,0 +1,6 @@
+/*
+* Basic arithmetic operations in java like + - * % /
+* Task:- take two numbers as input from user and print the addition, division (both quotient and remainder), multiplication, subtraction
+* */
+public class two {
+}
